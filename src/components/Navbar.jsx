e9a52ccad17b9cb6f5ea6db329bf7e47887ba099
Swayam-The Coder <swayamcode1@gmail.com>
2026-02-03 18:46:01 +0530

    best Sellers add functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
             <NavLink to="/" onClick={() => setOpen(false)}>
-            <img class="h-9" src={assets.logo} alt="logo"/>
+            <img className="h-9" src={assets.logo} alt="logo"/>
         </NavLink>
 
             {/* Desktop Menu */}
@@ -33,7 +33,7 @@ const Navbar = () => {
 
                 <div onClick={()=>navigate('/cart')} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-4 h-4 rounded-full">3</button>
                 </div>
 
                 {!user ? (<button onClick={()=>setshowuserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary transition text-white rounded-full">
@@ -57,7 +57,7 @@ const Navbar = () => {
 
             {/* Mobile Menu */}
             { open && (
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+            <div className={`${open ? 'flex' : 'hidden'} absolute left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to='/' onClick={()=> setOpen(false)}>Home</NavLink>
                 <NavLink to='/products' onClick={()=> setOpen(false)}>All products</NavLink>
                 {user &&
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,20 +1,72 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children}) =>{
+
+  const currency = import.meta.VITE_CURRENCY;
+
   const navigate = useNavigate();
   const [user, setuser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showuserLogin, setshowuserLogin] = useState(false);
-  
+  const [products, setproducts] = useState([]);
 
-  const value = {navigate, user, setuser,setIsSeller, isSeller, showuserLogin, setshowuserLogin}
+  const [ cartitem, setcartitems ] = useState({})
+
+  //Fetch all products
+  const fetchProducts = async ()=>{ 
+    setproducts(dummyProducts)
+  }
+
+  //Add products to cart
+  const addtocart = (itemId) => {
+    let cartData = structuredClone(cartitem)
+
+    if(cartData[itemId]){
+      cartData[itemId] += 1;
+    } else{
+      cartData[itemId] = 1;
+    }
+    setcartitems(cartData);
+    toast.success("Added to Cart")
+  }
+
+  //update card item quantity
+  const updatecart = (itemId, quantity) => {
+    let cartData = structuredClone(cartitem)
+    cartData[itemId] = quantity
+    setcartitems(cartData)
+    toast.success("Cart Updated")
+  }
+
+  //Remove items from cart
+  const removefromcart = (itemId) => {
+    let cartData = structuredClone(cartitem)
+    if(cartData[itemId]){
+      cartData[itemId] -= 1;
+      
+      if(cartData[itemId] === 0){
+        delete cartData[itemId]
+      }
+    }
+    toast.success("Removed from Cart")
+    setcartitems(cartData)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const value = {navigate, user, setuser,setIsSeller, isSeller, showuserLogin, setshowuserLogin, products, currency, addtocart, updatecart, removefromcart, cartitem }
 
   return <AppContext.Provider value={value}>
     {children}
   </AppContext.Provider>
+
 }
 
 export const useAppContext = () => {
