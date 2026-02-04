973a373051345ccce1d0c19c5686c6d26dd71d4b
Swayam-The Coder <swayamcode1@gmail.com>
2026-02-04 15:44:11 +0530

    search field updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
@@ -7,11 +7,17 @@ import { useAppContext }from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
-    const {user, setuser, setshowuserLogin, navigate} = useAppContext();
+    const {user, setuser, setshowuserLogin, navigate, searchQuery, setsearchQuery} = useAppContext();
     const logout = async ()=>{
         setuser(null);
         navigate('/')
     }
+
+    useEffect(()=>{
+        if(searchQuery.length > 0) {
+            navigate("/products")
+        }
+    },[searchQuery])
 
   return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
@@ -27,7 +33,7 @@ const Navbar = () => {
                 <NavLink to='/'>Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e)=> setsearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                     <img src={assets.search_icon} alt='search' className='w-4 h-4'/>
                 </div>
 
